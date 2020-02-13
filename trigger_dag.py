373c62f180b6a3bf2aa4680cd07cb1ddef90ef7d
adarapiyevich <adarapiyevich@griddynamics.com>
2020-02-13 13:30:24 +0300

    created dag for triggering 'process table flow'

---
+++ trigger_dag.py
@@ -0,0 +1,34 @@
+from airflow import DAG
+from airflow.contrib.sensors.file_sensor import FileSensor
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.dagrun_operator import TriggerDagRunOperator
+from datetime import datetime
+
+TRIGGER_FILE = '/Users/adarapiyevich/Workspace/dag_trigger'
+TRIGGER_DAG_ID = 'process_third_table'
+
+start_date = datetime(2020, 1, 1)
+
+default_args = {
+    'owner': 'adarapiyevich',
+    'start_date': start_date
+}
+
+with DAG(dag_id='trigger_table_update', schedule_interval=None, default_args=default_args) as dag:
+    wait_for_file_sensor = FileSensor(
+        task_id='wait_for_file',
+        filepath=TRIGGER_FILE,
+        poke_interval=5
+    )
+
+    trigger_process_table_dag_task = TriggerDagRunOperator(
+        task_id='trigger_process_table_dag',
+        trigger_dag_id=TRIGGER_DAG_ID
+    )
+
+    remove_trigger_file_task = BashOperator(
+        task_id='remove_trigger_file',
+        bash_command='rm -f {}'.format(TRIGGER_FILE)
+    )
+
+    wait_for_file_sensor >> trigger_process_table_dag_task >> remove_trigger_file_task
